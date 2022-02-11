ff418d883f26bcffe82b85d50e040a5fc7152145
lentyss <elenatur03@outlook.com>
2022-02-11 17:19:19 +0300

    work with card-list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('test');
+kolvo = document.getElementsByClassName('card').length;
+for (let i = 0; i < kolvo; i++){
+    if (i % 2 != 0){
+        document.getElementsByClassName('card')[i].style.backgroundColor = '#09c953';
+    }
+    document.getElementsByClassName('card-title')[i].innerHTML  += ' #' + (++i);
+    i--;
+}
+
+
