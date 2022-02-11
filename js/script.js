c1124c8d7ff35e2d5bae4e7c411da6e0507bcf9e
lentyss <elenatur03@outlook.com>
2022-02-11 17:30:41 +0300

    reworking

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 kolvo = document.getElementsByClassName('card').length;
-for (let i = 0; i < kolvo; i++){
+for (let i = 0; i < kolvo; i){
     if (i % 2 != 0){
         document.getElementsByClassName('card')[i].style.backgroundColor = '#09c953';
     }
     document.getElementsByClassName('card-title')[i].innerHTML  += ' #' + (++i);
-    i--;
 }
 
 
