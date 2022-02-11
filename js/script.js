fb394c854fb1815c8ca088014494dc4f9b6715a8
Константин <info@yroom.ru>
2022-02-11 17:25:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-kolvo = document.getElementsByClassName('card').length;
-for (let i = 0; i < kolvo; i++){
+const items = document.getElementsByClassName('card').length;
+for (let i = 0; i < items; i++){
+    let el = document.getElementsByClassName('card')[i];
     if (i % 2 != 0){
-        document.getElementsByClassName('card')[i].style.backgroundColor = '#09c953';
+        el.style.backgroundColor = '#09c953';
     }
-    document.getElementsByClassName('card-title')[i].innerHTML  += ' #' + (++i);
+    el.innerHTML  += ' #' + (++i);
     i--;
 }
 
